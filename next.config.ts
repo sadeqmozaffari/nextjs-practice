78d31aa5cab893dd0416d4c5b1d681766e91b8a2
Sadeq Mozaffari <sadeqmozaffari@gmail.com>
2024-12-09 09:34:36 +0430

    init nextjs app with prisma config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ['localhost', 'r4.wallpaperflare.com'],
+  },
 };
 
 export default nextConfig;
---
+++ src/app/pages/api/posts.ts
@@ -0,0 +1,67 @@
+import type {NextApiRequest, NextApiResponse} from 'next';
+import {PrismaClient} from '@prisma/client';
+const prisma = new PrismaClient();
+
+export default async function usersServices(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === 'GET') {
+    const {id} = req.query;
+    if (id) {
+      console.log(id);
+      const post = await prisma.post.findFirst({
+        where: {
+          id: Number(id),
+        },
+      });
+      if (!post) {
+        return res.json({error: 'Not found'});
+      }
+      res.json(post);
+    } else {
+      const posts = await prisma.post.findMany({
+        where: {
+          published: true,
+        },
+      });
+      res.json(posts);
+    }
+    res.statusCode = 400;
+    res.json({error: 'Bad request'});
+  }
+  if (req.method === 'POST') {
+    const {title, content, authorId, published} = req.body;
+    const post = await prisma.post.create({
+      data: {
+        title,
+        content,
+        authorId,
+        published,
+      },
+    });
+    return res.json(post);
+  }
+  if (req.method === 'PUT') {
+    let data = await prisma.post.update({
+      where: {
+        id: req.body.id,
+      },
+      data: {
+        title: req.body.title,
+        content: req.body.content,
+        published: req.body.published,
+      },
+    });
+    return res.json(data);
+  }
+  if (req.method === 'DELETE') {
+    let data = await prisma.post.delete({
+      where: {
+        id: req.body.id,
+      },
+    });
+    return res.json(data);
+  }
+  return res.json({});
+}
